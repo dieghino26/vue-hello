55c1d2e66f4dfdda584dc6272565f4669dad48c0
dieghino26 <brotzadiego@gmail.com>
2024-01-21 15:04:47 +0100

    added "hello" in page

--- js/vue.js
+++ js/vue.js
@@ -4,7 +4,11 @@ console.log("vue ok", Vue);
 //global build di Vue
 const {createApp} = Vue;
 const app = createApp({
-
+    data(){
+        return{
+            greetings: "hello"
+        }
+    }
 });
 
-app.mounth("#root")
+app.mount("#root");
